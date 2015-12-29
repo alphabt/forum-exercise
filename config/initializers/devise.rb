d1de6ea8dd58cc41fe0a2922f323ac047df35812
Brian Tai <alphabt@gmail.com>
2015-12-29 11:10:58 +0800

    Move Facebook OAuth key to config file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,7 +239,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "620908794716113", "2a9e6dd50165a70c9bb68b333546183c",
+
+  fb_config = Rails.application.config_for(:facebook)
+  config.omniauth :facebook, fb_config['app_id'],  fb_config['secret'],
                   scope: 'public_profile,email', info_fields: 'email,name'
 
   # ==> Warden configuration
